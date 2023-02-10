18b114e51ffeee4f54770231d095a219c47c7e8f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 14:37:58 +0600

    show image title and style

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     getPopular();
   }, []);
@@ -10,10 +13,38 @@ function Popular() {
       `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes);
   };
 
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <Wrapper key={recipe.id}>
+            <h3>Popular Picks</h3>
+            <Card>
+              <p>{recipe.title}</p>
+              <img src={recipe.image} alt={recipe.title} />
+            </Card>
+          </Wrapper>
+        );
+      })}
+    </div>
+  );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+`;
 
 export default Popular;
