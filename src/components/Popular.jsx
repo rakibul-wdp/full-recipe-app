6c83f5d0e0934fd7eec310c43771cfdf34752012
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 14:58:34 +0600

    add gradient

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -37,6 +37,7 @@ function Popular() {
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
@@ -81,6 +82,14 @@ const Card = styled.div`
     justify-content: center;
     align-items: center;
   }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;
