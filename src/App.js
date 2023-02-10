fa454feb954b68730ca6ddfc5be3d16c2162980b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 22:16:47 +0600

    create simple navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
+import { BrowserRouter, Link } from "react-router-dom";
+import styled from "styled-components";
 import Category from "./components/Category";
 import Search from "./components/Search";
 import Pages from "./pages/Pages";
@@ -6,11 +8,33 @@ import Pages from "./pages/Pages";
 function App() {
   return (
     <BrowserRouter>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={"/"} >Food Recipe</Logo>
+      </Nav>
       <Search />
       <Category />
       <Pages />
     </BrowserRouter>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
