a40c82e815a2dd40f2c000d93f9638bd3ac065d9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 18:26:09 +0600

    add some style on input field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
+import Search from "./components/Search";
 import Pages from "./pages/Pages";
 
 function App() {
   return (
     <BrowserRouter>
+      <Search />
       <Category />
       <Pages />
     </BrowserRouter>
