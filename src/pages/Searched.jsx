d2b33426ec3c364efd197d0584d6de95742cfe37
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 21:20:51 +0600

    show searched result in ui

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Searched() {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
@@ -17,7 +18,38 @@ function Searched() {
     getSearched(params.search);
   }, [params.search]);
 
-  return <div>Searched</div>;
+  return (
+    <Grid>
+      {searchedRecipes.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Searched;
