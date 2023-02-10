d1dff4eb02750ad554087c43664104ecec859b19
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 22:05:29 +0600

    learned how to render html tag from api with dangerouslySetInnerHTML and show details data

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 function Recipe() {
   const [details, setDetails] = useState({});
   let params = useParams();
+  const [activeTab, setActiveTab] = useState("instructions");
 
   const fetchDetails = async () => {
     const data = await fetch(
@@ -18,7 +19,41 @@ function Recipe() {
     fetchDetails();
   }, [params.name]);
 
-  return <div>{details.title}</div>;
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+      <Info>
+        <Button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients
+        </Button>
+        {activeTab === "instructions" && (
+          <div>
+            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+          </div>
+        )}
+        {activeTab === "ingredients" && (
+          <ul>
+            {details.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}>{ingredient.original}</li>
+            ))}
+          </ul>
+        )}
+      </Info>
+    </DetailWrapper>
+  );
 }
 
 const DetailWrapper = styled.div`
