124f4f2271a9cab08f6ac65d8ef680cd72d9d4af
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 21:41:25 +0600

    create recipe functionality

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Recipe() {
-  return <div>Recipe</div>;
+  const [details, setDetails] = useState({});
+  let params = useParams();
+
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+    const detailData = await data.json();
+    setDetails(detailData);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name]);
+
+  return <div>{details.title}</div>;
 }
 
 export default Recipe;
