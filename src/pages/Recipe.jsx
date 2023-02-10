a4fcd4a271e57df260f7246a7f172072507254b3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 21:47:08 +0600

    create styles for detials page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function Recipe() {
   const [details, setDetails] = useState({});
@@ -19,5 +20,42 @@ function Recipe() {
 
   return <div>{details.title}</div>;
 }
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+
+  h2 {
+    margin-bottom: 2rem;
+  }
+
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 
 export default Recipe;
