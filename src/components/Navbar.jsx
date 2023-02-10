dc6768877fc988ebeb9cffff590cf0894166d5f1
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-11 05:19:11 +0600

    create authentication system with auth0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,32 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { GiKnifeFork } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 function Navbar() {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
   return (
     <NavWrapper>
       <Nav>
         <GiKnifeFork />
         <Logo to={"/"}>Food Recipe</Logo>
       </Nav>
-      <Button>Login</Button>
+      <div>
+        <User>{isAuthenticated && user.name}</User>
+        {isAuthenticated ? (
+          <Button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </Button>
+        ) : (
+          <Button onClick={() => loginWithRedirect()}>Login</Button>
+        )}
+      </div>
     </NavWrapper>
   );
 }
@@ -39,6 +55,13 @@ const Nav = styled.div`
   }
 `;
 
+const User = styled.span`
+  margin-right: 1rem;
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: rgb(56, 56, 56);
+`;
+
 const Button = styled.button`
   background: linear-gradient(35deg, #494949, #313131);
   color: white;
@@ -47,6 +70,11 @@ const Button = styled.button`
   border-radius: 1rem;
   font-size: 1rem;
   font-weight: bold;
+  cursor: pointer;
+  &:hover {
+    background: linear-gradient(to right, #f27121, #e94057);
+    transition: 0.3s;
+  }
 `;
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,7 +6,15 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-s0t2brpud2p7n6km.us.auth0.com"
+    clientId="9LdSKIhlB5Jz9epOs3rteIuWYi3N1Mwd"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
