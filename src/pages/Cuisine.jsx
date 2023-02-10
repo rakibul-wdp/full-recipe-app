faf37d781fb86adb6c3c3d2df918dffc62fec4fd
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-10 22:08:59 +0600

    now all page show details

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 function Cuisine() {
@@ -23,8 +23,10 @@ function Cuisine() {
       {cuisine.map((item) => {
         return (
           <Card key={item.id}>
-            <img src={item.image} alt={item.title} />
-            <h4>{item.title}</h4>
+            <Link to={"/recipe/" + item.id}>
+              <img src={item.image} alt={item.title} />
+              <h4>{item.title}</h4>
+            </Link>
           </Card>
         );
       })}
